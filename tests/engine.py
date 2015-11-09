0630df1fcb47b7fb007aa55efc3e0ca83b121d48
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-11-09 14:40:57 +0800

    TESTS : Experiment in hitchtest unit testing - moved the py.test code into run.test

--- tests/engine.py
+++ tests/engine.py
@@ -2,6 +2,7 @@ from os import path, system, chdir
 from subprocess import check_call, call, PIPE, CalledProcessError
 import hitchpython
 import hitchtest
+import hitchcli
 
 
 # Get directory above this file
@@ -12,21 +13,49 @@ class ExecutionEngine(hitchtest.ExecutionEngine):
 
     def set_up(self):
         """Set up your applications and the test environment."""
+        chdir(PROJECT_DIRECTORY)
+        self.cli_steps = hitchcli.CommandLineStepLibrary()
+
+        self.run = self.cli_steps.run
+        self.expect = self.cli_steps.expect
+        self.send_control = self.cli_steps.send_control
+        self.send_line = self.cli_steps.send_line
+        self.finish = self.cli_steps.finish
+
         self.python_package = hitchpython.PythonPackage(
             self.preconditions.get('python_version', '3.5.0')
         )
         self.python_package.build()
 
-        chdir(PROJECT_DIRECTORY)
-
-        # Silently install py.test, ipython and flake8
-        check_call([self.python_package.pip, "install", "pytest"], stdout=PIPE)
-        check_call([self.python_package.pip, "install", "ipython"], stdout=PIPE)
+        # Silently install flake8
         check_call([self.python_package.pip, "install", "flake8"], stdout=PIPE)
 
         # Uninstall and reinstall
         call([self.python_package.pip, "uninstall", "dumbyaml", "-y"], stdout=PIPE)
         check_call([self.python_package.python, "setup.py", "install"], stdout=PIPE)
+
+        self.start_python_interpreter()
+        self.python_command("import dumbyaml")
+        self.python_command("import yaml")
+
+
+    def start_python_interpreter(self):
+        self.run(self.python_package.python)
+        self.expect(">>>")
+
+    def python_command(self, command=None, expect=None):
+        self.send_line(command)
+        if expect is not None:
+            self.expect(expect)
+        self.expect(">>>")
+
+    def assert_true(self, command):
+        self.python_command(command, expect="True")
+
+    def end_python_interpreter(self):
+        self.send_control("D")
+        self.finish()
+
 
     def flake8(self, directory):
         try:
@@ -47,3 +76,9 @@ class ExecutionEngine(hitchtest.ExecutionEngine):
             ])
         except CalledProcessError:
             raise RuntimeError("py.test failure")
+
+    def tear_down(self):
+        try:
+            self.end_python_interpreter()
+        except:
+            pass
